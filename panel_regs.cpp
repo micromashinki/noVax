fe927172ab54edb7582e479848395be5f27b859d
FedorVisa <71399241+FedorVisa@users.noreply.github.com>
2022-07-16 00:41:49 +0300

    Add files via upload

--- panel_regs.cpp
+++ panel_regs.cpp
@@ -6,12 +6,12 @@ RegPanel::RegPanel(Okno* o) : wxPanel() {
     size = wxSize((o->GetWidth() - o->GetWidth() / 1.5) - 26, ((o->GetHeight() - 250) / 1.5) - 5);
     
     Create(o, wxID_ANY, wxPoint((o->GetWidth() / 1.5) + 10, 0), size);
-    wxPanel* table_panel  = new wxPanel( this, wxID_ANY, wxPoint(0, 0), wxSize(175, 300));
-    wxPanel* table2_panel = new wxPanel(this, wxID_ANY, wxPoint(175,0), wxSize(200, 300));
-    table_panel->SetBackgroundColour(wxColor(255,140,105));
+    wxPanel* table_panel  = new wxPanel( this, wxID_ANY, wxPoint(0, 0), wxSize(150, 300));
+    wxPanel* table2_panel = new wxPanel(this, wxID_ANY, wxPoint(151,0), wxSize(200, 300));
+    table_panel ->SetBackgroundColour(wxColor(255, 140, 105));
     table2_panel->SetBackgroundColour(wxColor(230, 230, 250));
-    wxGrid* first_col = new wxGrid(table_panel, wxID_ANY, wxPoint(0, 50), wxSize(GetWidth(), GetHeight()));
-    wxGrid* sec_col = new wxGrid(table2_panel, wxID_ANY, wxPoint(0, 50), wxSize(GetWidth(), GetHeight()));
+    wxGrid* first_col = new wxGrid(table_panel, wxID_ANY, wxPoint(25, 75), wxSize(GetWidth(), GetHeight()));
+    wxGrid* sec_col = new wxGrid(table2_panel, wxID_ANY, wxPoint(25, 75), wxSize(GetWidth(), GetHeight()));
     wxTextCtrl* t_flag = new wxTextCtrl( this, wxID_ANY, "T = 0", wxPoint(20,  15), wxSize(50, 20), wxTE_READONLY );
     wxTextCtrl* n_flag = new wxTextCtrl( this, wxID_ANY, "N = 0", wxPoint(80,  15), wxSize(50, 20), wxTE_READONLY );
     wxTextCtrl* z_flag = new wxTextCtrl( this, wxID_ANY, "Z = 0", wxPoint(140, 15), wxSize(50, 20), wxTE_READONLY );
@@ -35,30 +35,72 @@ RegPanel::RegPanel(Okno* o) : wxPanel() {
         
         sec_col->SetRowLabelValue(i, wxString(_("R") + hex[i]));
     }
-    void RegPanel::denyResizeCol(wxGridSizeEvent & e) {
-        table->SetColSize(e.GetRowOrCol(), 39);
-        e.Veto();
 
-        //DELETE THIS ON PRODUCTION
-        wxMessageBox("??? ??????!\n??????");
-        //DELETE THIS ON PRODUCTION
-    }
-
-    void RegPanel::denyResizeRow(wxGridSizeEvent & e) {
-        table->SetRowSize(e.GetRowOrCol(), 25);
-        e.Veto();
-
-        //DELETE THIS ON PRODUCTION
-        wxMessageBox("??? ??????!\n??????");
-        //DELETE THIS ON PRODUCTION
-    }
+   
    
 }
 int RegPanel::GetWidth() { return size.GetWidth(); }
 int RegPanel::GetHeight() { return size.GetHeight(); }
 
+void RegPanel::checkValue(wxGridEvent& e) {
+    bool invalid = false;
+    wxString str = e.GetString();
+    int k = e.GetRow();
+    int j = e.GetCol();
+    if ((str.Length() > 2) or (str.IsEmpty())) {
+        invalid = true;
+        e.Veto();
+    }
+    else {
+        for (int i = 0; i < str.Length(); i++) {
+            if ((str[i] < '0') or (str[i] > '9')) {
+                if ((str[i] < 'A') or (str[i] > 'F')) {
+                    if ((str[i] < 'a') or (str[i] > 'f')) {
+                        e.Veto();
+                        invalid = true;
+                        break;
+                    }
+                    else {
+                        if (str[i] == 'a') { str[i] = 'A'; }
+                        else if (str[i] == 'b') { str[i] = 'B'; }
+                        else if (str[i] == 'c') { str[i] = 'C'; }
+                        else if (str[i] == 'd') { str[i] = 'D'; }
+                        else if (str[i] == 'e') { str[i] = 'E'; }
+                        else if (str[i] == 'f') { str[i] = 'F'; }
+                    }
+                }
+            }
+        }
+    }
+    if (!invalid) {
+        e.Veto();
+        if (str.length() == 1) { first_col->SetCellValue(k, j, wxString("0" + str)); }
+        else  first_col->SetCellValue(k, j, wxString(str));
+        /*int a;
+        if (k == 0) a = j;
+        else a = k*16+j;
+        setMemoryCell(a, std::stoi(str.ToStdString(), 0, 16));*/
+        setTheme(wxColour(0, 0, 0), wxColour(30, 30, 30), wxColour(102, 255, 0));
+    }
+}
+
+void RegPanel::denyResizeCol(wxGridSizeEvent& e) {
+    first_col->SetColSize(e.GetRowOrCol(), 39);
+    sec_col->SetColSize(e.GetRowOrCol(), 39);
+    e.Veto();
+
+}
+
+
+void RegPanel::denyResizeRow(wxGridSizeEvent& e) {
+    first_col->SetRowSize(e.GetRowOrCol(), 25);
+    sec_col->SetRowSize(e.GetRowOrCol(), 25);
+    e.Veto();
+}
+
+/*
 wxBEGIN_EVENT_TABLE(RegPanel, wxPanel)
 EVT_GRID_CELL_CHANGING(RegPanel::checkValue)
 EVT_GRID_COL_SIZE(RegPanel::denyResizeCol)
 EVT_GRID_ROW_SIZE(RegPanel::denyResizeRow)
-wxEND_EVENT_TABLE()
+wxEND_EVENT_TABLE()*/
--- panel_regs.h
+++ panel_regs.h
@@ -4,6 +4,12 @@ public:
 	RegPanel(Okno*);
 	int GetWidth();
 	int GetHeight();
+	void checkValue(wxGridEvent&);
+	void denyResizeCol(wxGridSizeEvent&);
+	void denyResizeRow(wxGridSizeEvent&);
+	void setTheme(const wxColour&, const wxColour&, const wxColour&, const wxColour& text = wxColour(102, 255, 0));
 private:
 	wxSize size;
+	wxGrid* first_col;
+	wxGrid* sec_col;
 };
