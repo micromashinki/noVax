61031ee8da2af9647a70d4bb591bb234bb57de97
Михаил Ярмолович <mishanay38@gmail.com>
2023-07-18 15:29:24 +0300

    added more vax tests. fixed some commands. added more jump and acb commands

--- noVaxEngine/tests/Cpocessor_vax_labs_tests.cpp
+++ noVaxEngine/tests/Cpocessor_vax_labs_tests.cpp
@@ -6,7 +6,7 @@
 #include "test.h"
 #include <cstdlib>
 
-/*
+
 TEST(Cprocessor, lab1) {
     Cprocessor proc1;
     proc1.load(PATH_Cprocessor_save_load "1lab_do.v11");
@@ -15,43 +15,38 @@ TEST(Cprocessor, lab1) {
     Cprocessor proc2;
     proc2.load(PATH_Cprocessor_save_load"1lab_posle.v11");
 
+    EXPECT_TRUE(proc1 == proc2);
+}
 
+TEST(Cprocessor, lab1_part_2) {
+    Cprocessor proc1;
+    proc1.load(PATH_Cprocessor_save_load "Lab_1_part_2_default.v11");
+    proc1.execute();
+
+    Cprocessor proc2;
+    proc2.load(PATH_Cprocessor_save_load"lab_1_part_2_after.v11");
 
     EXPECT_TRUE(proc1 == proc2);
-}*/
-
-TEST(Cprocessor, load_save_load_12) {
-    Cprocessor proc1;
-    Cprocessor proc2;
-    proc1.load(PATH_Cprocessor_save_load  "1.v11");
-    proc1.save(PATH_Cprocessor_save_load  "test_1.v11");
-    proc2.load(PATH_Cprocessor_save_load "test_1.v11");
-    EXPECT_EQ(proc1,proc2);
 }
 
-TEST(Cprocessor, load_22) {
+TEST(Cprocessor, lab3_part_1) {
     Cprocessor proc1;
-    proc1.load(PATH_Cprocessor_save_load "2.v11");
+    proc1.load(PATH_Cprocessor_save_load "lab3_part_1_default.v11");
+    proc1.execute();
 
     Cprocessor proc2;
-    proc2.setMemoryCell(0x0,0x80);
-    proc2.setMemoryCell(0x1,0x54);
-    proc2.setMemoryCell(0x2,0x54);
-    proc2.setMemoryCell(0x14E,0x56);
-    proc2.setMemoryCell(0x14F,0x66);
-    proc2.setRegisterCell(1,0x99);
-    proc2.setRegisterCell(2,0x32);
-    proc2.setRegisterCell(4,0x54);
-    proc2.serFlags(0,1,1,1);
+    proc2.load(PATH_Cprocessor_save_load"lab3_part_1_after.v11");
 
-    EXPECT_EQ(proc1,proc2);
+    EXPECT_TRUE(proc1 == proc2);
 }
 
-TEST(Cprocessor, load_save_load_22) {
+TEST(Cprocessor, lab3_part_2) {
     Cprocessor proc1;
+    proc1.load(PATH_Cprocessor_save_load "lab3_part_2_default.v11");
+    proc1.execute();
+
     Cprocessor proc2;
-    proc1.load(PATH_Cprocessor_save_load "2.v11");
-    proc1.save(PATH_Cprocessor_save_load "test_2.v11");
-    proc2.load(PATH_Cprocessor_save_load "test_2.v11");
-    EXPECT_EQ(proc1,proc2);
+    proc2.load(PATH_Cprocessor_save_load"lab3_part_2_after.v11");
+
+    EXPECT_TRUE(proc1 == proc2);
 }
